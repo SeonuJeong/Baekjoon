178e9b6795f62b948719790799a2bb55abe7ff6d
SeonuJeong <65950955+SeonuJeong@users.noreply.github.com>
2022-11-07 08:52:34 +0900

    [Silver II] Title: 안녕, Time: 0 ms, Memory: 2028 KB -BaekjoonHub

--- 백준/Silver/1535. 안녕/안녕.cc
+++ 백준/Silver/1535. 안녕/안녕.cc
@@ -13,13 +13,18 @@ const int INF = 1e9;
 int N;
 vector<int> hp;
 vector<int> ej;
-
+int dp[101][21];
 
 int go(int h,int pos){
 	//cout<<"h: "<<h<<" pos: "<<pos<<"\n";
 	if(h<=0){
 		return 0;
 	}
+	
+	int& ret=dp[h][pos];
+	
+	if(ret!=-1)
+		return ret;
 	
 	int val=0;
 	rep(i,pos+1,N){
@@ -28,12 +33,13 @@ int go(int h,int pos){
 		val=max(val,go(h-hp[i],i)+ej[i]);
 	}
 	
+	ret=val;
 	return val;
 }
 
 void solve(){
 	cin>>N;
-	
+	memset(dp,-1,sizeof(dp));
 	rep(i,0,N){
 		int c;cin>>c;
 		hp.push_back(c);
