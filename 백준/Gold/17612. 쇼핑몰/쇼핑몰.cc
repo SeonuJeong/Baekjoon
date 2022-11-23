649993e5f52a395ddc451698bfa2e12d035cbfcd
SeonuJeong <65950955+SeonuJeong@users.noreply.github.com>
2022-11-23 23:01:04 +0900

    [Gold II] Title: 쇼핑몰, Time: 52 ms, Memory: 13420 KB -BaekjoonHub

---
+++ 백준/Gold/17612. 쇼핑몰/쇼핑몰.cc
@@ -0,0 +1,64 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define rep(i,x,n) for(int i=x;i<n;i++)
+#define f first
+#define s second
+
+typedef long long ll;
+typedef pair<ll, ll> pi;
+typedef tuple<ll,ll,ll> ti;
+const int INF = 1e9;
+
+int N,k;
+vector<pi> v;
+priority_queue<ti,vector<ti>,greater<ti>> minheap;
+priority_queue<ti,vector<ti>,greater<ti>> pq;
+void solve(){
+	cin>>N>>k;
+	
+	rep(i,0,N){
+		ll id,w; cin>>id>>w;
+		v.push_back({id,w});
+	}
+
+	for(ll i=0;i<N&&i<k;i++)
+		minheap.push(make_tuple(v[i].s,i+1,v[i].f));
+	
+	ll ret=0;
+	ll cnt=1;
+	rep(i,k,N){
+		ll w,id,kth;
+		tie(w,kth,id)=minheap.top();
+		minheap.pop();
+		
+		minheap.push(make_tuple(w+v[i].s, kth,v[i].f));
+		pq.push(make_tuple(w,-kth,id));
+	}
+	
+	while(minheap.size()){
+		ll w,id,kth;
+		tie(w,kth,id)=minheap.top();
+		minheap.pop();
+		pq.push(make_tuple(w,-kth,id));
+	}
+	
+	while(pq.size()){
+		ll w,id,kth;
+		tie(w,kth,id)=pq.top();
+		pq.pop();
+		ret+=cnt*id;
+		cnt++;
+	}
+	
+	cout<<ret;
+}
+
+int main() {
+
+	ios_base::sync_with_stdio(false);
+    cout.tie(nullptr);
+    cin.tie(nullptr);
+
+	solve();
+}
