4ad22760c2d4d60bb6a034ce4092896a057846c2
SeonuJeong <65950955+SeonuJeong@users.noreply.github.com>
2022-11-24 16:29:01 +0900

    [Gold II] Title: 직각다각형, Time: 0 ms, Memory: 2428 KB -BaekjoonHub

--- 백준/Gold/17611. 직각다각형/직각다각형.cc
+++ 백준/Gold/17611. 직각다각형/직각다각형.cc
@@ -1,37 +1,91 @@
-#include <bits/stdc++.h>
-using namespace std; 
-#define y1 fuck
-vector<int> check_x, check_y; 
-int n, x, y, x1, y1, x2, y2, _y[1000004], _x[1000004], ret; 
-pair<int, int> a[100004];
+#include<bits/stdc++.h>
+using namespace std;
+
+#define rep(i,x,n) for(int i=x;i<n;i++)
+#define f first
+#define s second
+
+typedef long long ll;
+typedef pair<int, int> pi;
+typedef tuple<int,int,int> ti;
+const int INF = 1e9;
+
+
+int n;
+vector<pi> v;
+vector<pi> xv;
+vector<pi> yv;
+
+void solve(){
+	cin>>n;
+	
+	rep(i,0,n){
+		int x,y; cin>>x>>y;
+		v.push_back({x,y});
+	}
+	
+	
+	rep(i,1,n){
+		if(v[i-1].f==v[i].f){
+			yv.push_back({min(v[i-1].s,v[i].s),max(v[i-1].s,v[i].s)});
+		}
+		else{
+			xv.push_back({min(v[i-1].f,v[i].f),max(v[i-1].f,v[i].f)});
+		}
+	}
+	
+	if(v[0].f==v[n-1].f){
+		yv.push_back({min(v[0].s,v[n-1].s),max(v[0].s,v[n-1].s)});
+	}
+	else{
+		xv.push_back({min(v[0].f,v[n-1].f),max(v[0].f,v[n-1].f)});
+	}
+		
+	
+	//minheap
+	priority_queue<int,vector<int>,greater<int>> pq;
+	
+	int ret=1;
+	sort(xv.begin(),xv.end());
+	pq.push(xv[0].s);
+	rep(i,1,xv.size()){
+		
+		while(!pq.empty()&&pq.top()<=xv[i].f){
+			pq.pop();
+		}
+		
+		pq.push(xv[i].s);
+		
+		int tmp=pq.size();
+		ret=max(ret, tmp);
+	}
+	
+	while(!pq.empty())
+		pq.pop();
+	
+	
+	sort(yv.begin(),yv.end());
+	pq.push(yv[0].s);
+	rep(i,1,yv.size()){
+		
+		while(!pq.empty()&&pq.top()<=yv[i].f){
+			pq.pop();
+		}
+		
+		pq.push(yv[i].s);
+		
+		int tmp=pq.size();
+		ret=max(ret,tmp);
+	}
+
+	cout<<ret;
+}
+
 int main() {
-    cin >> n; 
-    for(int i = 0; i < n; i++){
-        cin >> x >> y;
-        x += 500000;
-        y += 500000;
-        a[i] = {x, y};  
-        check_x.push_back(x);  
-        check_y.push_back(y);  
-    }
-    a[n] = a[0]; 
-    for(int i = 0; i < n; i++){
-        tie(x1, y1) = a[i];
-        tie(x2, y2) = a[i + 1];  
-        if(x1 != x2){
-            if(x1 > x2) swap(x1, x2); 
-            for(int j = x1 + 1; j <= x2; j++){
-                _x[j]++; 
-            }
-        }
-        else if(y1 != y2){
-            if(y1 > y2) swap(y1, y2); 
-            for(int j = y1 + 1; j <= y2; j++){
-                _y[j]++; 
-            }
-        }
-    } 
-    for(int a : check_y) ret = max(ret, _y[a]); 
-    for(int a : check_x) ret = max(ret, _x[a]);   
-    cout << ret << "\n";
+
+	ios_base::sync_with_stdio(false);
+    cout.tie(nullptr);
+    cin.tie(nullptr);
+
+	solve();
 }
