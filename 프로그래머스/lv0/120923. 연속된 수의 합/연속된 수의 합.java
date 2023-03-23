067e1359efc0b79bcd16cfc5ad07d5c7b11a91d7
SeonuJeong <jung48273@gmail.com>
2023-03-23 17:14:50 +0900

    [level 0] Title: 연속된 수의 합, Time: 0.02 ms, Memory: 73.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120923. 연속된 수의 합/연속된 수의 합.java
+++ 프로그래머스/lv0/120923. 연속된 수의 합/연속된 수의 합.java
@@ -1,18 +1,11 @@
-import java.util.*;
 class Solution {
     public int[] solution(int num, int total) {
-        int[] answer = {};
-        List<Integer> ans = new ArrayList<>();
-        
-        int start = ((total*2/num)+1-num)/2;
-        
-        for(int i=0;i<num;i++){
-            ans.add(start+i);
-        }
-        
-        answer = ans.stream().mapToInt(Integer::intValue).toArray();
+        int[] answer = new int[num];
+        int start = (total*2/num+1-num)/2;
+
+        for(int i=0;i<num;i++)
+            answer[i]=start+i;
         
         return answer;
-        
     }
 }
